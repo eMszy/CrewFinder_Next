f16325b081095a61638d7c74bf2970bcd9355ef4
eMszy <vilonya@gmail.com>
2022-03-04 17:29:37 +0100

    try

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 import { AuthContext } from "../context/auth-context";
 import { autoLogin } from "../shared/autoLogin";
 
-import Login from "../components/login/login";
+import Login from "../components/login/Login";
 import { Calendar } from "../components/Calendar/Calendar";
 import Sidebar from "../components/Sidebar/Sidebar";
 
