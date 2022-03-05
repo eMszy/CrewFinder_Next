696476e9fdc42da74b41e81b4ed482308eca537d
eMszy <vilonya@gmail.com>
2022-03-05 12:28:10 +0100

    update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	env: {
-		MONGO_URL: "mongodb://localhost/CrewFinderDev",
-		SECRET_WORD: "somesupersecretsecret",
+		// MONGO_URL: "mongodb://localhost/CrewFinderDev",
+		// SECRET_WORD: "somesupersecretsecret",
 		GRAPHQL: "http://localhost:3000/api/graphql",
 		GOOGLE_SINGIN_CLIENT_ID:
 			"621211722177-j4er2ugj63vkq9hm1fhrvndpmqr4qp22.apps.googleusercontent.com",
