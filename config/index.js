a7cdf9ffa917ae9382e731308ca88d000fd3c8dd
eMszy <vilonya@gmail.com>
2022-06-13 11:24:08 +0200

    server https fix

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
 	? "http://localhost:3000"
-	: "https://crew-finder-next.vercel.app/";
+	: "https://crewfindernext.herokuapp.com";
